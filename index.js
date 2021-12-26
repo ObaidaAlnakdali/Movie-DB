6bfd65ea1af24850e4b0da77c46edc78a04dfc07
obaida <o.alnakdali@gmail.com>
2021-12-26 20:05:20 +0200

    Step 3

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
+const date = new Date()
+const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : '' + date.getMinutes();
+const hours = date.getHours()
+const TIME = hours + ":" + minutes
+
 app.get('/', function (req, res) {
-  res.send('ok')
+    res.send('ok')
+})
+
+app.get('/test', function (req, res) {
+    res.send({ status: 200, message: "ok" })
+})
+
+app.get('/time', function (req, res) {
+    res.send({status:200, message:TIME})
 })
 
 app.listen(3000)
