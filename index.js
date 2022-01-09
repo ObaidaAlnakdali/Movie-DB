1fbcd742ccdafddbe9c9f1e1b86498027a326a9b
obaida <o.alnakdali@gmail.com>
2022-01-09 11:33:33 +0200

    step 13

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
+const verify = require('./routes/verifyToken');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 var cors = require('cors')
@@ -12,6 +12,7 @@ dotenv.config()
 const authRoute = require('./routes/auth');
 
 //Route Middlewares
+app.use(bodyParser.json())
 app.use('/api/user', authRoute)
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
@@ -30,7 +31,7 @@ const movies = mongoose.model("movies", movieSchema);
 //////////  CRUD   Movie  ////////////
 //////////////////////////////////////
 
-app.post('/movies/add', function (req, res) {
+app.post('/movies/add', verify, function (req, res) {
     if (req.query.title != "" && req.query.title != undefined && req.query.year != 0 && req.query.year != undefined && req.query.year.length == 4 && !isNaN(req.query.year)) {
         newMovie = { title: req.query.title, year: req.query.year, rating: req.query.rating == "" || req.query.rating == undefined ? 4 : req.query.rating }
         movies.create(newMovie).then(movie => res.status(200).json(movie))
@@ -39,16 +40,16 @@ app.post('/movies/add', function (req, res) {
     }
 })
 
-app.get('/movies/get', function (req, res) {
+app.get('/movies/get', verify, function (req, res) {
     movies.find().then(movie => res.status(200).json(movie))
 })
 
-app.get('/movies/get/id/:id', function (req, res) {
+app.get('/movies/get/id/:id', verify, function (req, res) {
     movies.findById(req.params.id).then(movie => res.status(200).json(movie))
         .catch(error => res.status(404).send(`{status:404, error:true, message:'the movie ${req.params.id} does not exist'}`))
 })
 
-app.get('/movies/get/by-date', function (req, res) {
+app.get('/movies/get/by-date', verify, function (req, res) {
     function compare(a, b) {
         if (a.year > b.year) {
             return -1;
@@ -60,7 +61,7 @@ app.get('/movies/get/by-date', function (req, res) {
     movies.find().then(movie => res.status(200).json(movie.sort(compare)));
 })
 
-app.get('/movies/get/by-rating', function (req, res) {
+app.get('/movies/get/by-rating', verify, function (req, res) {
     movieList = movies;
     function compare(a, b) {
         if (a.rating > b.rating) {
@@ -73,7 +74,7 @@ app.get('/movies/get/by-rating', function (req, res) {
     movies.find().then(movie => res.status(200).json(movie.sort(compare)));
 })
 
-app.get('/movies/get/by-title', function (req, res) {
+app.get('/movies/get/by-title', verify, function (req, res) {
     movieList = movies;
     function compare(a, b) {
         if (a.title < b.title) {
@@ -86,7 +87,7 @@ app.get('/movies/get/by-title', function (req, res) {
     movies.find().then(movie => res.status(200).json(movie.sort(compare)));
 })
 
-app.put('/movies/edit/:id', function (req, res) {
+app.put('/movies/edit/:id', verify, function (req, res) {
     movies.findById(req.params.id).then(movie => {
         movie.title = req.query.title != undefined || req.query.title != "" ? req.query.title : movie.title
         movie.year = req.query.year != 0 && req.query.year != undefined && req.query.year.length == 4 && !isNaN(req.query.year) ? req.query.year : movie.year
@@ -96,7 +97,7 @@ app.put('/movies/edit/:id', function (req, res) {
     }).catch(error => res.status(404).send(error))
 })
 
-app.delete('/movies/delete/:id', function (req, res) {
+app.delete('/movies/delete/:id', verify, function (req, res) {
     movies.findOneAndDelete({ _id: req.params.id })
         .then(movie => res.status(200).json(movie + "the movie is deleted"))
         .catch(error => res.status(404).send(`the id is not correct`))
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,67 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken') ;
+const bcrypt = require('bcryptjs');
+const Joi = require('@hapi/joi');
+
+
+// const schema = Joi.object({
+//     username: Joi.string().min(5).required,
+//     password: Joi.string().min(8).required
+// })
+
+const schema = Joi.object({
+    username: Joi.string().min(5).required(),
+    password: Joi.string().min(8).required()
+});
 
 router.post('/signup', async (req, res) => {
-    // const user = new User({
-    //     username : req.body.user,
-    //     password : req.body.password
-    // })
-    res.send(req.body.username, req.body.password)
+
+    //validate data entry to new user
+    const { error } = schema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    //validate if the user is exist or not
+    const usernameExist = await User.findOne({ username: req.body.username })
+    if (usernameExist) return res.status(400).send("the username is exists")
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    //create a new user
+    const user = new User({
+        username: req.body.username,
+        password: hashedPassword
+    })
+
+    //add to user to database
     try {
-        //const newUser = await user.save()
-        // res.status(200).send({
-        //     id: newUser._id,
-        //     username: newUser.username
-        //   })
-      } 
-      catch (error) {
-        res.status(404).send({message: error.message})
-      }
+        const newUser = await user.save()
+        res.status(200).json({ newUser })
+    }
+    catch (error) {
+        res.status(404).send({ message: error.message })
+    }
 })
 
-router.post('/signin', (req, res) => {
-    res.send("signin")
+router.post('/signin', async (req, res) => {
+    const user = await User.findOne({ username: req.body.username })
+    //validate data entry to new user
+    const { error } = schema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    //Check if username is exsist
+    if (!user) return res.status(400).send("the username is not exsist");
+
+    // Check if password is correct
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(400).send({ message: 'the password is wrong' })
+
+    // if all information is true
+    // Create a token and assign it to the user
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
 })
 
 
